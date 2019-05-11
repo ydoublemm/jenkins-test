8f7625da1100fa536e3b4141e08936bd8136c970
ymm <871049074@qq.com>
2019-05-11 10:41:25 +0800

    Initial commit

---
+++ src/main/java/com/ymm/jenkinstest/HelloController.java
@@ -0,0 +1,19 @@
+package com.ymm.jenkinstest;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: ymm
+ * @Date: 2019-05-11 10:35 
+ * @Description:
+ *
+ */
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String hello(){
+		return "jenkins测试";
+	}
+}
