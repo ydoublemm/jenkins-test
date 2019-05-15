6b798bef1da624658d0d300105c7215960032511
ymm <871049074@qq.com>
2019-05-15 15:28:19 +0800

    =================================

--- src/main/java/com/ymm/jenkinstest/HelloController.java
+++ src/main/java/com/ymm/jenkinstest/HelloController.java
@@ -15,11 +15,6 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String hello(){
-		System.out.println("123");
-		System.out.println("1");
-		System.out.println("2");
-		System.out.println("3");
-		System.out.println("1");
 		return "jenkins测试";
 	}
 }
